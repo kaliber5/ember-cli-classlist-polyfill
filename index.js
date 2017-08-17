33d622e1added4d6f94e2a884b3e6d33f74deaac
simonihmig <ihmig@kaliber5.de>
2017-08-17 14:31:30 +0200

    Add polyfill for `this.import` to work with older ember-cli versions as well

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const path = require('path');
 const fastbootTransform = require('fastboot-transform');
 const Funnel = require('broccoli-funnel');
 
+function findHostShim() {
+  let current = this;
+  let app;
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+  return app;
+}
+
 module.exports = {
   name: 'ember-cli-classlist-polyfill',
 
-  included(app) {
+  included() {
 
-    // @todo add support for older ember-cli
-    this.import('vendor/classlist-polyfill/index.js');
+    // polyfill `this.import` if required
+    let _import = this.import || function(asset, options) {
+      let app = findHostShim.call(this);
+      app.import(asset, options);
+    };
+
+    _import.call(this, 'vendor/classlist-polyfill/index.js');
   },
 
   treeForVendor() {
