94509965b08645aa1d82532c001c7c9e9664c011
simonihmig <ihmig@kaliber5.de>
2017-08-17 13:45:07 +0200

    Initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const fastbootTransform = require('fastboot-transform');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-cli-classlist-polyfill'
+  name: 'ember-cli-classlist-polyfill',
+
+  included(app) {
+
+    // @todo add support for older ember-cli
+    this.import('vendor/classlist-polyfill/index.js');
+  },
+
+  treeForVendor() {
+    let modulePath = path.dirname(require.resolve('classlist-polyfill'));
+    let vendorTree = new Funnel(modulePath, {
+      destDir: 'classlist-polyfill'
+    });
+
+    return fastbootTransform(vendorTree);
+  }
 };
